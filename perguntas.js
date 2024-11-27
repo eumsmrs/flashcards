a0923820b649ef8208e1d4647f437035b13d0414
eumsmrs <esquecedaniel@gmail.com>
2024-11-26 22:12:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Musica',
+    'O que é o funk',
+    'um estilo musical'
+)
+/* Luiz.Daniel.Lima.2c.15/11/2024
+*/
+criaCartao(
+    'Futebol',
+    'O que é uma pedalada?',
+    'pedalada é um drible no futebol.'
+)
+
+criaCartao(
+    'Brincadeira',
+    'O que é o Pedra,papel e tesoura?',
+    'Uma brincadeira onde as mãos sao usadas'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
